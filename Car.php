3c4a316ba3fafb8b8d8bbe3fccf7f810037143fe
Joseph Karasek and Josh Overly <joseph.karasek@gmail.com>
2016-02-08 09:19:12 -0800

    add constructor with default values to Car class

--- Car.php
+++ Car.php
@@ -1,38 +1,33 @@
 <?php
 class Car
 {
-    public $make_model;
-    public $price;
-    public $miles;
+
+    function worthBuying($max_price)
+    {
+        return $this->price < ($max_price + 100);
+    }
+
+    function __construct ($make_model,$price=100000,$miles=0)
+    {
+        $this->make_model = $make_model;
+        $this->price = $price;
+        $this->miles = $miles;
+    }
 }
 
+$porsche = new Car("2014 Porsche 911", 114991, 7854);
+$ford = new Car("2011 Ford F450", 55995, 14241);
+$lexus = new Car("2013 Lexus RX 350", 44700, 20000);
+$mercedes = new Car("Mercedes Benz CLS550", 39900, 37979);
+$chevy = new Car("Chevy");
+$geo = new Car("Geo", 2000);
 
-$porsche = new Car();
-$porsche->make_model = "2014 Porsche 911";
-$porsche->price = 114991;
-$porsche->miles = 7864;
-
-$ford = new Car();
-$ford->make_model = "2011 Ford F450";
-$ford->price = 55995;
-$ford->miles = 14241;
-
-$lexus = new Car();
-$lexus->make_model = "2013 Lexus RX 350";
-$lexus->price = 44700;
-$lexus->miles = 20000;
-
-$mercedes = new Car();
-$mercedes->make_model = "Mercedes Benz CLS550";
-$mercedes->price = 39900;
-$mercedes->miles = 37979;
-
-$cars = array($porsche, $ford, $lexus, $mercedes);
+$cars = array($porsche, $ford, $lexus, $mercedes, $chevy, $geo);
 
 $cars_matching_search = array();
 
 foreach ($cars as $car) {
-    if ($car->price < $_GET["price"]) {
+    if ($car->worthBuying($_GET["price"])) {
         array_push($cars_matching_search, $car);
     }
 }
